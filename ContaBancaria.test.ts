f6727996c2768145204de4b2bc54606f666052cb
Leticiabeatriz15 <leticiabeatriz1508@gmail.com>
2024-09-18 15:03:31 -0300

    Entrega de atividade

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,44 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
 
+describe("Testando classe ContaBancaria", () => {
+  let conta: ContaBancaria;
+  let conta2: ContaBancaria;
+  beforeEach(() => {
+    conta = new ContaBancaria();
+    conta2 = new ContaBancaria();
+    
+  });
+
+  test("Testando depositar valor positivo", () => {
+    conta.depositar(100);
+    expect(conta.consultarSaldo()).toBe(100);
+  });
+
+  test("Testando depositar valor negativo", () => {
+    conta.depositar(-100);
+    expect(conta.consultarSaldo()).toBe(0);
+  });
+
+  test("Deve sacar um valor com saldo suficiente", () => {
+    conta.depositar(100);
+    expect(conta.sacar(40));
+    expect(conta.consultarSaldo()).toBe(60);
+  });
+
+  test("Deve lançar um erro ao tentar sacar com saldo insuficiente", () => {
+    conta.depositar(100);
+    conta.sacar(150);
+    expect(conta.consultarSaldo()).toBe(100);
+  });
+
+  test("Testando o valor da transferência", () => {
+    conta.depositar(100);
+    conta.transferencia(50, conta2);
+    expect(conta.consultarSaldo()).toBe(50);
+    expect(conta2.consultarSaldo()).toBe(50);
+  
+  })
+
+});
 
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,46 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+    private numeroConta : number = 2
+    private agenciaConta : number = 3
+    private saldoConta = 0
+    private extratoConta : string[] = []
+
+    public depositar(valor: number){
+        if(valor > 0){
+            this.saldoConta += valor
+            this.extratoConta.push(`Valor depositado: ${valor.toFixed(2)}`)  
+        }else{
+            console.log('Valor inválido')
+        }
+    }
+
+    public sacar(valorSaque: number){
+        if(valorSaque <= this.saldoConta && valorSaque > 0){
+            this.saldoConta -= valorSaque
+            this.extratoConta.push(`Valor sacado: ${valorSaque.toFixed(2)}`)
+        }
+        else{
+            console.log("Saque inválido")
+        }
+    }
+
+    public transferencia(valorT: number, contaDestino: ContaBancaria){
+        if(valorT <= this.saldoConta){
+            this.saldoConta -= valorT
+            contaDestino.depositar(valorT)
+            this.extratoConta.push(`Valor transferido: ${valorT.toFixed(2)} Agência: ${this.agenciaConta} Número da conta: ${this.numeroConta}`)
+        }else {
+            console.log("Valor Inválido!")
+        }
+    } 
+    
+    public consultarSaldo(){
+        console.log(this.saldoConta)
+        return this.saldoConta
+    }
+    
+    public exibeExtrato(){
+        console.log(`Extrato da Conta: ${this.numeroConta}`)
+        this.extratoConta.forEach(transacoes => {console.log(transacoes)})
+        console.log(`Saldo atual: ${this.saldoConta.toFixed(2)}`)
+    }
+}
